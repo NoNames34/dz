6e176908f943ed0769cfe639fb2ef68709c48cb4
AntonVN <ridick4@yandex.ru>
2023-03-02 23:08:27 +0500

    dz2-rename

---
+++ zadacha-2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+var a = Decimal.Parse(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            var b = Decimal.Parse(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine(a);
+            else 
+                Console.WriteLine(b);
