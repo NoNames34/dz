591e7d4e83854f12c79a13dae98a3f7aca928d89
AntonVN <ridick4@yandex.ru>
2023-03-02 23:21:21 +0500

    dz6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Проверяем чётность. Введите число: ");
+           int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " : нечётное");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + " : чётное");
+            }
