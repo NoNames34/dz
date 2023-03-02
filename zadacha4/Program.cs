43c842ec1d33e70c2f92fdeaddc6f7977d23fb04
AntonVN <ridick4@yandex.ru>
2023-03-02 23:05:26 +0500

    dz4_new

--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Дайте 3 числа:");
+            int num_a= Convert.ToInt32(Console.ReadLine());
+            int num_b = Convert.ToInt32(Console.ReadLine());
+            int num_c = Convert.ToInt32(Console.ReadLine());
+
+            int max = num_a;
+
+            if (num_b > max)
+            {
+                max = num_b;
+            }
+
+            if (num_c > max)
+            {
+                max = num_c;
+            }
+Console.WriteLine("Наибольшее из данных чисел = " + max);
