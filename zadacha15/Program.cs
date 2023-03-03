73fd7d3a40742ba03cb6c966323ff099e2f9d78f
AntonVN <ridick4@yandex.ru>
2023-03-04 03:33:12 +0500

    dz15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите численное значение дня недели: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) {
+	if (Day >= 6) {
+		Console.Write("Ура, выходной!!!");
+	} else {
+		Console.Write("Будний день");
+	}
+} else {
+	Console.Write("Такого дня нет, в неделе всего 7 дней");
+}
