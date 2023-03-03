c1ab7c46d7649693661b16f734359cfbe09877f9
AntonVN <ridick4@yandex.ru>
2023-03-04 03:13:38 +0500

    dz10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3) {
+	int result = ( Value/10 ) % 10;
+	Console.WriteLine("Вторая цифра данного числа " + result);
+} else {
+	Console.WriteLine("Число не трёхзначное!");
+}
