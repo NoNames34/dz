7be8cf360cf47a790c1834d6c8d0aec3d86e9cc9
AntonVN <ridick4@yandex.ru>
2023-03-04 03:25:47 +0500

    dz13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число с количеством цифр не менее 3-х: ");
+int Value2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = Value2.ToString().Length;
+if (Length2 >= 3) {
+	while (Value2 > 999)
+	{
+		Value2 = Value2 / 10;
+	}
+	int result = Value2 % 10;
+	Console.WriteLine("Третья цифра числа " + result);
+} else {
+	Console.WriteLine("в числе нет ретьей цифры!");
+}
