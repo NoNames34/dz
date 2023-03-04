fd8ecc055221b472d5e9f1bd24e10ed0805c8368
AntonVN <ridick4@yandex.ru>
2023-03-05 00:36:38 +0500

    dz41

---
+++ zadacha41/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Укажите несколько чисел через пробел: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во чисел больше нуля: {count}");
