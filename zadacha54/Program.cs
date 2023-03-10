f6476680d05b873e4ba95b296fc595e018e98ccd
AntonVN <ridick4@yandex.ru>
2023-03-11 00:13:54 +0500

    dz54

---
+++ zadacha54/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Clear();
+Console.Write("Введите размерность m массива: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m,n];
+
+void mas(int m, int n)
+    {
+    int i,j;
+    Random rand = new Random();
+    for (i = 0; i < m; i++)
+        {
+        for (j = 0; j < n; j++)
+            {
+            randomArray[i,j] = rand.Next(1,9);
+            }
+        }
+    }
+
+void printm(int[,] array)
+    {
+    int i,j;
+    for (i = 0; i < array.GetLength(0); i++)
+        {
+        Console.WriteLine();
+        for (j = 0; j < array.GetLength(1); j++)
+            {
+            Console.Write($"{array[i,j]} ");
+            }
+        Console.WriteLine();
+        }
+    }
+
+mas(m,n);
+Console.WriteLine("Массив: ");
+printm(randomArray);
+
+void sortirovka(int[,] array)
+    {
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        for (int j = 0; j < array.GetLength(1); j++)
+            {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+                {
+                if (array[i, k] < array[i, k + 1])
+                    {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+sortirovka(randomArray);
+Console.WriteLine("Отсортированный массив: ");
+printm(randomArray);
